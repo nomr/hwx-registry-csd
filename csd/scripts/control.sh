45e883c932baab4ac8fa7b4dfe36c0dfeddd87be
Patrick Sodré <sodre@sodre.co>
2017-12-02 20:29:05 +0000

    Migrated start script options into CSD
      - Java Heap Options
      - Java Performance Options
      - CSD Java Options

--- csd/scripts/control.sh
+++ csd/scripts/control.sh
@@ -6,11 +6,11 @@ set -efu -o pipefail
 
 locate_java_home
 
-shell_format=""
+shell_format="\$ZK_QUORUM"
 for i in ${!HWX_REGISTRY_*}; do
   shell_format="${shell_format},\$$i"
 done
-
 cat ${CONF_DIR}/registry.yaml | envsubst $shell_format > ${CONF_DIR}/registry.yaml.final
 
+export REGISTRY_OPTS=$CSD_JAVA_OPTS
 exec ${HWX_REGISTRY_HOME}/bin/registry-server-start.sh ${CONF_DIR}/registry.yaml.final
