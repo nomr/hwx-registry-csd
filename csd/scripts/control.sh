4a35adcd06aa3a675e604fa43b8990572bbc9d8b
Patrick Sodré <sodre@sodre.co>
2017-12-02 22:19:39 +0000

    Add ZK_ROOT option and simplify envsubst code

--- csd/scripts/control.sh
+++ csd/scripts/control.sh
@@ -4,13 +4,21 @@ set -efu -o pipefail
 
 . ${COMMON_SCRIPT}
 
-locate_java_home
 
-shell_format="\$ZK_QUORUM"
-for i in ${!HWX_REGISTRY_*}; do
-  shell_format="${shell_format},\$$i"
-done
-cat ${CONF_DIR}/registry.yaml | envsubst $shell_format > ${CONF_DIR}/registry.yaml.final
+envsubst_all() {
+    shell_format="\$ZK_QUORUM"
+    for i in ${!HWX_REGISTRY_*}; do
+        shell_format="${shell_format},\$$i"
+    done
+
+    for i in $(find . -maxdepth 1 -type f -name '*.envsubst*'); do
+        cat $i | envsubst $shell_format > ${i/\.envsubst/}
+        rm -f $i
+    done
+}
+
+locate_java_home
+envsubst_all
 
 export REGISTRY_OPTS=$CSD_JAVA_OPTS
 exec ${HWX_REGISTRY_HOME}/bin/registry-server-start.sh ${CONF_DIR}/registry.yaml.final
