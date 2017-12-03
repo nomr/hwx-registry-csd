8d8d009b867577c17bad2d51202a6040629cedbf
Patrick Sodré <sodre@sodre.co>
2017-12-03 02:07:04 +0000

    Add initial stateful storage providers

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Cloudera Service Descriptor for NiFi TLS Infrastructure
 
 [![Build Status](https://travis-ci.org/nomr/nifi-tls-csd.svg?branch=master)](https://travis-ci.org/nomr/nifi-tls-csd)
+
+
+## Storage Provider
+
+### PostgreSQL
+```sql
+CREATE USER "hwx-registry" WITH LOGIN ENCRYPTED PASSWORD 'password';
+CREATE DATABASE "hwx-registry" OWNER "hwx-registry";
+```
--- csd/scripts/control.sh
+++ csd/scripts/control.sh
@@ -4,9 +4,8 @@ set -efu -o pipefail
 
 . ${COMMON_SCRIPT}
 
-
 envsubst_all() {
-    shell_format="\$ZK_QUORUM"
+    local shell_format="\$ZK_QUORUM"
     for i in ${!HWX_REGISTRY_*}; do
         shell_format="${shell_format},\$$i"
     done
@@ -17,8 +16,41 @@ envsubst_all() {
     done
 }
 
-locate_java_home
-envsubst_all
+append_and_delete() {
+    local in=$1
+    local out=$2
+    if [ -e $in ]; then
+      cat $in >> $out
+      rm -f $in
+    fi
+}
 
+mv_if_exists() {
+    if [ -e ${in} ]; then
+      mv $1 $2
+    fi
+}
+
+move_aux_files() {
+    local in=aux/registry-sp-${HWX_REGISTRY_SP}.envsubst.yaml 
+    local out=${CONF_DIR}/registry-sp.envsubst.yaml
+    mv_if_exists $in $out
+}
+
+edit_variables() {
+    HWX_REGISTRY_SP_DB_PORT=":${HWX_REGISTRY_SP_DB_PORT}"
+    if [ "${HWX_REGISTRY_SP_DB_PORT}" == ":0" ]; then
+        HWX_REGISTRY_SP_DB_PORT=""
+    fi
+}
+
+
+
+edit_variables
+move_aux_files
+envsubst_all
+append_and_delete ${CONF_DIR}/registry-sp.yaml ${CONF_DIR}/registry.yaml
+
+locate_java_home
 export REGISTRY_OPTS=$CSD_JAVA_OPTS
-exec ${HWX_REGISTRY_HOME}/bin/registry-server-start.sh ${CONF_DIR}/registry.yaml.final
+exec ${HWX_REGISTRY_HOME}/bin/registry-server-start.sh ${CONF_DIR}/registry.yaml
