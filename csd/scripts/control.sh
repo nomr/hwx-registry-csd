0c84e6a8565aea31419587b2fe92718aea2a1fa0
Patrick Sodré <sodre@sodre.co>
2017-12-03 23:38:42 +0000

    Add Kerberos support for Zookeeper connections

--- csd/scripts/control.sh
+++ csd/scripts/control.sh
@@ -32,7 +32,7 @@ mv_if_exists() {
 }
 
 move_aux_files() {
-    local in=aux/registry-sp-${HWX_REGISTRY_SP}.envsubst.yaml 
+    local in=aux/registry-sp-${HWX_REGISTRY_SP}.envsubst.yaml
     local out=${CONF_DIR}/registry-sp.envsubst.yaml
     mv_if_exists $in $out
 
@@ -54,14 +54,18 @@ load_variables() {
         HWX_REGISTRY_SP_DB_PORT=""
     fi
 
+    # Kerberos Variables
+    export HWX_REGISTRY_KEYTAB=${CONF_DIR}/${HWX_REGISTRY_USER/-/_}.keytab
+
     # Logging Variables
-    export HWX_REGISTRY_LOG_THRESHOLD=$(get_property log4j log.threshold)
-    export HWX_REGISTRY_LOG_FILE=$(get_property log4j log.file)
-    export HWX_REGISTRY_LOG_FILE_COUNT=$(get_property log4j max.log.file.backup.index)
-    export HWX_REGISTRY_LOG_FILE_SIZE=$(get_property log4j max.log.file.size)
-    export HWX_REGISTRY_LOG_FILE_FORMAT=$(get_property log4j log4j.appender.RFA.layout.ConversionPattern)
-    export HWX_REGISTRY_LOG_CONSOLE_FORMAT=$(get_property log4j log4j.appender.console.layout.ConversionPattern)
-    rm -f log4j.properties
+    if [ -e log4j.properties ]; then
+        export HWX_REGISTRY_LOG_THRESHOLD=$(get_property log4j log.threshold)
+        export HWX_REGISTRY_LOG_FILE=$(get_property log4j log.file)
+        export HWX_REGISTRY_LOG_FILE_COUNT=$(get_property log4j max.log.file.backup.index)
+        export HWX_REGISTRY_LOG_FILE_SIZE=$(get_property log4j max.log.file.size)
+        export HWX_REGISTRY_LOG_FILE_FORMAT=$(get_property log4j log4j.appender.RFA.layout.ConversionPattern)
+        export HWX_REGISTRY_LOG_CONSOLE_FORMAT=$(get_property log4j log4j.appender.console.layout.ConversionPattern)
+    fi
 
 }
 
@@ -80,6 +84,11 @@ registry_main() {
 
     locate_java_home
     export REGISTRY_OPTS=$CSD_JAVA_OPTS
+
+    if [ -e ${HWX_REGISTRY_KEYTAB} ]; then
+        REGISTRY_OPTS="${REGISTRY_OPTS} -Djava.security.auth.login.config=jaas.conf"
+    fi
+
     exec ${HWX_REGISTRY_HOME}/bin/registry-server-start.sh ${CONF_DIR}/registry.yaml
 }
 
@@ -99,6 +108,30 @@ bootstrap_main() {
     done
 }
 
+zookeeper_main() {
+    if [ -z ${ZK_QUORUM+x} ]; then
+        return 127
+    fi
+
+    create_registry_yaml
+    locate_java_home
+
+    export CLIENT_JVMFLAGS=""
+    ZK_ACL="world:anyone:cdrwa"
+    if [ -e ${HWX_REGISTRY_KEYTAB} ]; then
+        CLIENT_JVMFLAGS="$CLIENT_JVMFLAGS -Djava.security.auth.login.config=jaas.conf"
+        ZK_ACL="sasl:${HWX_REGISTRY_USER}:cdrwa,world:anyone:r"
+    fi
+    case "$1" in
+        create)
+            zookeeper-client -server $ZK_QUORUM create ${HWX_REGISTRY_ZK_ROOT} "Created by Cloudera Manager" $ZK_ACL
+            ;;
+        drop)
+            zookeeper-client -server $ZK_QUORUM rmr ${HWX_REGISTRY_ZK_ROOT}
+            ;;
+    esac
+}
+
 program=$1
 shift
 case "$program" in
@@ -108,8 +141,11 @@ case "$program" in
     bootstrap)
       bootstrap_main $@
       ;;
+    zookeeper)
+      zookeeper_main $@
+      ;;
     *)
-      echo "Usage: control.sh <registry|bootstrap>"
+      echo "Usage: control.sh <registry|bootstrap|zookeeper>"
       exit 127
       ;;
 esac
